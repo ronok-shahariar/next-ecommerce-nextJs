3d579750f26e8d6814c86c66db43f9748ada1683
ronok-shahariar <346ronokarya@gmail.com>
2024-11-29 14:57:41 +0600

    adding New products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,12 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        {/* <ProductList /> */}
+        <Suspense fallback={"loading"}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
     </div>
   );
